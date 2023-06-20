ad7f8a24d2a8c58e551fdab2f08f6c001e870368
QAC-Khan <QAC.Khan@gmail.com>
2023-06-20 19:30:45 +0100

    First Commit

---
+++ src/main/java/abc/com/ProjectX/ProjectXApplication.java
@@ -0,0 +1,13 @@
+package abc.com.ProjectX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectXApplication.class, args);
+	}
+
+}
